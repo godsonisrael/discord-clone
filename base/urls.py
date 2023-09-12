b280b8130e4b2c070d966221cdf8bae3b9013cd2
Godson <37632179+godsonisrael@users.noreply.github.com>
2023-09-12 20:20:13 +0530

    wip

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
     path('', views.home, name="home"),
     path('room/<str:key>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
--- base/views.py
+++ base/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Room, Topic
+from django.shortcuts import render, redirect
 from .forms import RoomForm
+from .models import Room, Topic
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
 
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username, password=password)
+        except:
+            messages.error(request, 'Invalid username or password')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
-    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q) )
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
     topics = Topic.objects.all()
     rooms_count = rooms.count()
     context = {'rooms': rooms, 'topics': topics, 'rooms_count': rooms_count}
